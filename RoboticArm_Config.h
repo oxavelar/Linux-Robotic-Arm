9472ab9a003745f9cce3b28093c02f821c174087
Omar Avelar <omar.avelar@intel.com>
2015-08-11 10:13:48 -0500

    Test...

--- RoboticArm_Config.h
+++ RoboticArm_Config.h
@@ -6,21 +6,22 @@
  *
  * The following is Intel's Galileo layout for the pins.
  *
- *   +==========+=========+=======================+
- *   |  SYS_FS  |   LABEL |           DESCRIPTION |
- *   +==========+=========+=======================+
- *   |  gpio24  |     IO6 |       QE Channel A #1 |
- *   |  gpio25  |    IO11 |       QE Channel B #1 |
- *   |  gpio26  |     IO8 |       QE Channel A #2 |
- *   |  gpio27  |     IO7 |       QE Channel B #2 |
- *   |    pwm3  |     IO3 |   Motor DC Ctrl CW #1 |
- *   |    pwm7  |    IO10 |  Motor DC Ctrl CCW #1 |
- *   |    pwm1  |     IO9 |   Motor DC Ctrl CW #2 |
- *   |    pwm5  |     IO5 |  Motor DC Ctrl CCW #2 |
- *   |  video0  | USB HST |         USB HD WebCam |
- *   +==========+=========+====================== +
+ *   +==========+=========+=======================+==========
+ *   |  SYS_FS  |   LABEL |           DESCRIPTION |   COLOR |
+ *   +==========+=========+=======================+==========
+ *   |  gpio24  |     IO6 |       QE Channel A #1 |   White |
+ *   |  gpio25  |    IO11 |       QE Channel B #1 |  Yellow |
+ *   |  gpio26  |     IO8 |       QE Channel A #2 |   White |
+ *   |  gpio27  |     IO7 |       QE Channel B #2 |  Yellow |
+ *   |    pwm3  |     IO3 |   Motor DC Ctrl CW #1 |     Red |
+ *   |    pwm7  |    IO10 |  Motor DC Ctrl CCW #1 |    Blue |
+ *   |    pwm1  |     IO9 |   Motor DC Ctrl CW #2 |     Red |
+ *   |    pwm5  |     IO5 |  Motor DC Ctrl CCW #2 |    Blue |
+ *   |  video0  | USB HST |         USB HD WebCam |         |
+ *   +==========+=========+====================== +=========+
  *
  * Note: Galileo's cannot go slower than ~125 Hz on Linux SYSFS PWM.
+ * Note: Pololu encoders cable colors match the ones above.
  * 
  * The quadrature encoders in this project are a Pololu brand ones.
  * one offers 48 CPR in a 75:1 configuration, while the other is a
