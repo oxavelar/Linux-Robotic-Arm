093706a4360833c5119f06b10ed0df43c27487ae
Omar Avelar <omar.avelar@intel.com>
2015-08-19 11:19:18 -0500

    Putting the toolbox in a new namespace.

--- toolbox.h
+++ toolbox.h
@@ -6,18 +6,22 @@
 
 
 #define appname "Linux-Robotic-Arm"
-#define logger std::cout << appname << ": " << timestamp().c_str() << ": "
+#define logger std::cout << appname << ": " << toolbox::timestamp().c_str() << ": "
 
-inline const std::string timestamp(void)
+namespace toolbox
 {
-    time_t now = time(0);
-    struct tm tstruct;
-    char buf[80];
-    tstruct = *localtime(&now);
-    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
-    // for more information about date/time format
-    strftime(buf, sizeof(buf), "%X", &tstruct);
 
-    return buf;
+    inline const std::string timestamp(void)
+    {
+        time_t now = time(0);
+        struct tm tstruct;
+        char buf[80];
+        tstruct = *localtime(&now);
+        // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+        // for more information about date/time format
+        strftime(buf, sizeof(buf), "%X", &tstruct);
+    
+        return buf;
+    }
 }
 
