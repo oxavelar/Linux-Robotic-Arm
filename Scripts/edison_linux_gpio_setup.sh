7f55214e663a2291121cec8c3805315b26ba77fe
oxavelar <omar.avelar@gmail.com>
2017-05-09 18:55:08 -0500

    960 ohms only applie to I2C pins, using 2k

--- Scripts/edison_linux_gpio_setup.sh
+++ Scripts/edison_linux_gpio_setup.sh
@@ -79,7 +79,7 @@ echo 225 > /sys/class/gpio/unexport
 
 echo "I: Enabling GPIO48 on IO7"
 echo mode0 > /sys/kernel/debug/gpio_debug/gpio48/current_pinmux
-echo 20k > /sys/kernel/debug/gpio_debug/gpio48/current_pullstrength
+echo 2k > /sys/kernel/debug/gpio_debug/gpio48/current_pullstrength
 echo pulldown > /sys/kernel/debug/gpio_debug/gpio48/current_pullmode
 echo disable > /sys/kernel/debug/gpio_debug/gpio48/current_opendrain
 echo no-override > /sys/kernel/debug/gpio_debug/gpio48/current_override_outdir
@@ -87,7 +87,7 @@ echo override-enable > /sys/kernel/debug/gpio_debug/gpio48/current_override_indi
 
 echo "I: Enabling GPIO49 on IO8"
 echo mode0 > /sys/kernel/debug/gpio_debug/gpio49/current_pinmux
-echo 20k > /sys/kernel/debug/gpio_debug/gpio49/current_pullstrength
+echo 2k > /sys/kernel/debug/gpio_debug/gpio49/current_pullstrength
 echo pulldown > /sys/kernel/debug/gpio_debug/gpio49/current_pullmode
 echo disable > /sys/kernel/debug/gpio_debug/gpio49/current_opendrain
 echo no-override > /sys/kernel/debug/gpio_debug/gpio49/current_override_outdir
@@ -101,7 +101,7 @@ echo 226 > /sys/class/gpio/export
 echo high > /sys/class/gpio/gpio263/direction
 echo low > /sys/class/gpio/gpio240/direction
 echo mode0 > /sys/kernel/debug/gpio_debug/gpio41/current_pinmux
-echo 20k > /sys/kernel/debug/gpio_debug/gpio41/current_pullstrength
+echo 2k > /sys/kernel/debug/gpio_debug/gpio41/current_pullstrength
 echo pulldown > /sys/kernel/debug/gpio_debug/gpio41/current_pullmode
 echo disable > /sys/kernel/debug/gpio_debug/gpio41/current_opendrain
 echo no-override > /sys/kernel/debug/gpio_debug/gpio41/current_override_outdir
@@ -121,7 +121,7 @@ echo 227 > /sys/class/gpio/export
 echo high > /sys/class/gpio/gpio262/direction
 echo low > /sys/class/gpio/gpio241/direction
 echo mode0 > /sys/kernel/debug/gpio_debug/gpio43/current_pinmux
-echo 20k > /sys/kernel/debug/gpio_debug/gpio43/current_pullstrength
+echo 2k > /sys/kernel/debug/gpio_debug/gpio43/current_pullstrength
 echo pulldown > /sys/kernel/debug/gpio_debug/gpio43/current_pullmode
 echo disable > /sys/kernel/debug/gpio_debug/gpio43/current_opendrain
 echo no-override > /sys/kernel/debug/gpio_debug/gpio43/current_override_outdir
